c9e030fa3a4d65313e0ddd105296efb1ccc7d298
carykh <carykh@stanford.edu>
2021-05-17 06:02:02 -0700

    Update random.py

--- code/exampleStrats/random.py
+++ code/exampleStrats/random.py
@@ -1,9 +1,9 @@
 import random
 
-# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+# Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
 def strategy(history, memory):
     if random.randint(0,1) == 0:
-        return "side with accomplice", None
+        return "stay silent", None
     else:
-        return "side with law", None
+        return "tell truth", None
