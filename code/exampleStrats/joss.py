bc7dd2f5fe08680f43f7727f1caa457814dcaae6
carykh <carykh@stanford.edu>
2021-05-16 17:17:15 -0700

    Update joss.py

--- code/exampleStrats/joss.py
+++ code/exampleStrats/joss.py
@@ -3,9 +3,11 @@ import random
 # Variant of Tit For Tat that randomly defects to try to take advantage
 # of overly forgiving opponents.
 
+# Reminder: For the history array, "cooperate" = 1, "defect" = 0
+
 def strategy(history, memory):
-    choice = 1
+    choice = "cooperate"
     if random.random() < 0.10 or (history.shape[1] >= 1 and history[1,-1] == 0):
     # Choose to defect randomly by 10% chance, OR if and only if the opponent just defected.
-        choice = 0
+        choice = "defect"
     return choice, None
