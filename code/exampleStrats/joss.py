8d4266087ab0f5ed3e88bcd363118f829dda525f
carykh <carykh@stanford.edu>
2021-05-17 06:01:37 -0700

    Update joss.py

--- code/exampleStrats/joss.py
+++ code/exampleStrats/joss.py
@@ -1,13 +1,13 @@
 import random
 
-# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+# Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
-# Variant of Tit For Tat that randomly defects to try to take advantage
+# Variant of Tit For Tat that randomly "tells the truth" to try to take advantage
 # of overly forgiving opponents.
 
 def strategy(history, memory):
-    choice = "side with accomplice"
+    choice = "stay silent"
     if random.random() < 0.10 or (history.shape[1] >= 1 and history[1,-1] == 0):
-    # Choose to side-with-law randomly by 10% chance, OR if and only if the opponent just defected.
-        choice = "side with law"
+    # Choose to tell the truth randomly by 10% chance, OR if and only if the opponent just told the truth.
+        choice = "tell truth"
     return choice, None
