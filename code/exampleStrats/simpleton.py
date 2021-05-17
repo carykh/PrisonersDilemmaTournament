2c09ab1a82b73a92e8cb32cfe25755397f4c2492
carykh <carykh@stanford.edu>
2021-05-16 17:54:42 -0700

    Update simpleton.py

--- code/exampleStrats/simpleton.py
+++ code/exampleStrats/simpleton.py
@@ -1,18 +1,20 @@
 # Strategy described in Nicky Case's "The Evolution of Trust"
 # https://ncase.me/trust/
-#
-# SIMPLETON: Hi! I try to start by cooperating. If  you cooperate
+
+# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+
+# SIMPLETON: Hi! I try to start by "siding with accomplice". If you "side with accomplice"
 # back, I do the same thing as my last move, even if it was a mistake.
-# If you cheat back, I do the opposite thing as my last move, even
+# If you "side with law" back, I do the opposite thing as my last move, even
 # if it was a mistake.
 
 def strategy(history, memory):
     choice = None
     if history.shape[1] == 0: # We're on the first turn!
-        choice = 1
+        choice = "side with accomplice"
     else:
-        choice = history[0,-1] # I will keep doing the same thing as last move!
-        if history[1,-1] == 0: # If my opponent defected last turn, I'll just do the opposite thing as my last move:
-            choice = 1-choice
+        choice = "side with accomplice" if history[0,-1] == 1 else "side with law" # I will keep doing the same thing as last move!
+        if history[1,-1] == 0: # If my opponent "sided with law" last turn, I'll just do the opposite thing as my last move:
+            choice = "side with law" if history[0,-1] == 1 else "side with accomplice"
             
     return choice, None
