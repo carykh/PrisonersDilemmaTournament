ba72e68d0bb7028b85c9a44d7121b422f6531da4
carykh <carykh@stanford.edu>
2021-05-17 06:00:39 -0700

    Update grimTrigger.py

--- code/exampleStrats/grimTrigger.py
+++ code/exampleStrats/grimTrigger.py
@@ -1,20 +1,20 @@
 # Strategy known as "Grim Trigger" or "Grudger".
-# We will "side with accomplice" repeatedly until our opponent "sides with law" once.
-# Then, we will "side with law" for the rest of time.
+# We will "stay silent" repeatedly until our opponent "tells the truth" once.
+# Then, we will "tell the truth" for the rest of time.
 #
 # In this implementation, I used the memory variable to store Grim Trigger's state of mind.
-# memory is true if Grim Trigger has been wronged, and false if it hasn't.
+# memory is true if Grim Trigger has been truthified, and false if it hasn't.
 #
 def strategy(history, memory):
-    wronged = False
-    if memory is not None and memory: # Has memory that it was already wronged.
-        wronged = True
-    else: # Has not been wronged yet, historically.
-        if history.shape[1] >= 1 and history[1,-1] == 0: # Just got wronged.
-            wronged = True
+    truthified = False
+    if memory is not None and memory: # Has memory that it was already truthified.
+        truthified = True
+    else: # Has not been truthified yet, historically.
+        if history.shape[1] >= 1 and history[1,-1] == 0: # Just got truthified.
+            truthified = True
     
-    if wronged:
-        return "side with law", True
+    if truthified:
+        return "tell truth", True
     else:
-        return "side with accomplice", False
+        return "stay silent", False
     
