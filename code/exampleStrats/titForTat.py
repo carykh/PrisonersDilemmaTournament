fb5528030a2c4bc4ef82ec551848fb2c1a78e5ad
carykh <carykh@stanford.edu>
2021-05-17 06:04:15 -0700

    Update titForTat.py

--- code/exampleStrats/titForTat.py
+++ code/exampleStrats/titForTat.py
@@ -1,7 +1,7 @@
-# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+# Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
 def strategy(history, memory):
-    choice = "side with accomplice"
-    if history.shape[1] >= 1 and history[1,-1] == 0: # Choose to "side with law" if and only if the opponent also just "sided with law".
-        choice = "side with law"
+    choice = "stay silent"
+    if history.shape[1] >= 1 and history[1,-1] == 0: # Choose to "tell the truth" if and only if the opponent also just "told the truth".
+        choice = "tell truth"
     return choice, None
