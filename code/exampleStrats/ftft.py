3559ace37a93b13a7918aaaeb46e3ac597f26673
carykh <carykh@stanford.edu>
2021-05-16 17:11:29 -0700

    Update ftft.py

--- code/exampleStrats/ftft.py
+++ code/exampleStrats/ftft.py
@@ -1,7 +1,10 @@
 # Forgiving Tit for Tat.
 # Choose to defect if and only if the opponent just defected TWICE in a row.
+
+# Reminder: For the history array, "cooperate" = 1, "defect" = 0
+
 def strategy(history, memory):
-    choice = 1
+    choice = "cooperate"
     if history.shape[1] >= 2 and history[1,-1] == 0 and history[1,-2] == 0: # We check the TWO most recent turns to see if BOTH were defections, and only then do we defect too.
-        choice = 0
+        choice = "defect"
     return choice, None
