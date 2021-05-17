896141ab1cc1ce86b46e91841c53c6cb1b7523fb
carykh <carykh@stanford.edu>
2021-05-17 06:03:13 -0700

    Update simpleton.py

--- code/exampleStrats/simpleton.py
+++ code/exampleStrats/simpleton.py
@@ -1,20 +1,20 @@
 # Strategy described in Nicky Case's "The Evolution of Trust"
 # https://ncase.me/trust/
 
-# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+# Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
-# SIMPLETON: Hi! I try to start by "siding with accomplice". If you "side with accomplice"
+# SIMPLETON: Hi! I try to start by "staying silent". If you "stay silent"
 # back, I do the same thing as my last move, even if it was a mistake.
-# If you "side with law" back, I do the opposite thing as my last move, even
+# If you "tell the truth" back, I do the opposite thing as my last move, even
 # if it was a mistake.
 
 def strategy(history, memory):
     choice = None
     if history.shape[1] == 0: # We're on the first turn!
-        choice = "side with accomplice"
+        choice = "stay silent"
     else:
-        choice = "side with accomplice" if history[0,-1] == 1 else "side with law" # I will keep doing the same thing as last move!
-        if history[1,-1] == 0: # If my opponent "sided with law" last turn, I'll just do the opposite thing as my last move:
-            choice = "side with law" if history[0,-1] == 1 else "side with accomplice"
+        choice = "stay silent" if history[0,-1] == 1 else "tell truth" # I will keep doing the same thing as last move!
+        if history[1,-1] == 0: # If my opponent "told the truth" last turn, I'll just do the opposite thing as my last move:
+            choice = "tell truth" if history[0,-1] == 1 else "stay silent"
             
     return choice, None
