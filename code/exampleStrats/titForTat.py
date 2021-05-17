558c567d104671a1b10f8a73e85db17804523771
carykh <carykh@stanford.edu>
2021-05-16 17:57:11 -0700

    Update titForTat.py

--- code/exampleStrats/titForTat.py
+++ code/exampleStrats/titForTat.py
@@ -1,5 +1,7 @@
+# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+
 def strategy(history, memory):
-    choice = 1
-    if history.shape[1] >= 1 and history[1,-1] == 0: # Choose to defect if and only if the opponent just defected.
-        choice = 0
+    choice = "side with accomplice"
+    if history.shape[1] >= 1 and history[1,-1] == 0: # Choose to "side with law" if and only if the opponent also just "sided with law".
+        choice = "side with law"
     return choice, None
