f04b45f30b985e975c53faa94e9265e506a87d6a
carykh <carykh@stanford.edu>
2021-05-17 05:59:24 -0700

    Update ftft.py

--- code/exampleStrats/ftft.py
+++ code/exampleStrats/ftft.py
@@ -1,9 +1,9 @@
-# Reminder: For the history array, "side with law" = 0, "side with accomplice" = 1
+# Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
 # Forgiving Tit for Tat.
-# Choose to side with law if and only if the opponent just sided with the law TWICE in a row.
+# Choose to tell the truth if and only if the opponent just told the truth TWICE in a row.
 def strategy(history, memory):
-    choice = "side with accomplice"
-    if history.shape[1] >= 2 and history[1,-1] == 0 and history[1,-2] == 0: # We check the TWO most recent turns to see if BOTH were law-sidings, and only then do we side with the law too.
-        choice = "side with law"
+    choice = "stay silent"
+    if history.shape[1] >= 2 and history[1,-1] == 0 and history[1,-2] == 0: # We check the TWO most recent turns to see if BOTH were truth-tellings, and only then do we also tell the truth.
+        choice = "tell truth"
     return choice, None
