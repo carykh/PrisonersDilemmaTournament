c7bf671301f11c25e5f5d86c83bb8e790f033da8
valadaptive <valadaptive@protonmail.com>
2021-05-20 01:42:03 -0400

    Add "fool me twice" strategy

---
+++ code/valadaptive/foolMeTwice.py
@@ -0,0 +1,12 @@
+# We will cooperate repeatedly until our opponent betrays us twice.
+# Then, we will get angry and defect for the rest of time.
+
+def strategy(history, wrongedCount):
+    if wrongedCount is None:
+        wrongedCount = 0
+    if history.shape[1] >= 1 and history[1, -1] == 0:  # Just got wronged.
+        wrongedCount += 1
+
+    choice = 0 if wrongedCount >= 2 else 1
+
+    return choice, wrongedCount
