e20280cda8f7046cc30cc74058a911e205111ce1
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2021-05-20 16:48:36 +0700

    little refactor to support various game setting

--- code/prisonersDilemma.py
+++ code/prisonersDilemma.py
@@ -4,24 +4,6 @@ import importlib
 import numpy as np
 import random
 
-STRATEGY_FOLDER = "exampleStrats"
-RESULTS_FILE = "results.txt"
-
-pointsArray = [[1,5],[0,3]] # The i-j-th element of this array is how many points you receive if you do play i, and your opponent does play j.
-moveLabels = ["D","C"]
-# D = defect,     betray,       sabotage,  free-ride,     etc.
-# C = cooperate,  stay silent,  comply,    upload files,  etc.
-
-
-# Returns a 2-by-n numpy array. The first axis is which player (0 = us, 1 = opponent)
-# The second axis is which turn. (0 = first turn, 1 = next turn, etc.
-# For example, it might have the values
-#
-# [[0 0 1]       a.k.a.    D D C
-#  [1 1 1]]      a.k.a.    C C C
-#
-# if there have been 3 turns, and we have defected twice then cooperated once,
-# and our opponent has cooperated all three times.
 def getVisibleHistory(history, player, turn):
     historySoFar = history[:,:turn].copy()
     if player == 1:
@@ -35,14 +17,25 @@ def strategyMove(move):
     else:
         return move
 
-def runRound(pair):
+def runRound(STRATEGY_FOLDER, pair, minGameLength=200, logMultiplier=40):
+    """
+    Returns a 2-by-n numpy array. The first axis is which player (0 = us, 1 = opponent)
+    The second axis is which turn. (0 = first turn, 1 = next turn, etc.
+    For example, it might have the values
+    
+    [[0 0 1]       a.k.a.    D D C
+     [1 1 1]]      a.k.a.    C C C
+    
+    if there have been 3 turns, and we have defected twice then cooperated once,
+    and our opponent has cooperated all three times.
+    """
     moduleA = importlib.import_module(STRATEGY_FOLDER+"."+pair[0])
     moduleB = importlib.import_module(STRATEGY_FOLDER+"."+pair[1])
     memoryA = None
     memoryB = None
     
-    LENGTH_OF_GAME = int(200-40*np.log(random.random())) # The games are a minimum of 50 turns long. The np.log here guarantees that every turn after the 50th has an equal (low) chance of being the final turn.
-    history = np.zeros((2,LENGTH_OF_GAME),dtype=int)
+    LENGTH_OF_GAME = int(minGameLength-logMultiplier*np.log(random.random())) # The games are a minimum of 50 turns long. The np.log here guarantees that every turn after the 50th has an equal (low) chance of being the final turn.
+    history = np.zeros((2,LENGTH_OF_GAME),dtype=int) # history(i,:) for player i
     
     for turn in range(LENGTH_OF_GAME):
         playerAmove, memoryA = moduleA.strategy(getVisibleHistory(history,0,turn),memoryA)
@@ -52,7 +45,12 @@ def runRound(pair):
         
     return history
     
-def tallyRoundScores(history):
+def tallyRoundScores(history, pointsArray=[[1,5],[0,3]]):
+    """
+    pointsArray: 2 x 2 points table
+
+    The i-j-th element of this array is how many points you receive if you do play i, and your opponent does play j.
+    """
     scoreA = 0
     scoreB = 0
     ROUND_LENGTH = history.shape[1]
@@ -64,6 +62,10 @@ def tallyRoundScores(history):
     return scoreA/ROUND_LENGTH, scoreB/ROUND_LENGTH
     
 def outputRoundResults(f, pair, roundHistory, scoresA, scoresB):
+    moveLabels = ["D","C"] # 1: D, 2: C
+    # D = defect,     betray,       sabotage,  free-ride,     etc.
+    # C = cooperate,  stay silent,  comply,    upload files,  etc.
+
     f.write(pair[0]+" (P1)  VS.  "+pair[1]+" (P2)\n")
     for p in range(2):
         for t in range(roundHistory.shape[1]):
@@ -80,25 +82,30 @@ def pad(stri, leng):
         result = result+" "
     return result
     
-def runFullPairingTournament(inFolder, outFile):
-    print("Starting tournament, reading files from "+inFolder)
-    scoreKeeper = {}
+def fetch_strategy(inFolder):
     STRATEGY_LIST = []
     for file in os.listdir(inFolder):
         if file.endswith(".py"):
             STRATEGY_LIST.append(file[:-3])
-            
-            
+    return STRATEGY_LIST
+
+def runFullPairingTournament(inFolder, outFile):
+    print("Starting tournament, reading files from "+inFolder)
+    STRATEGY_LIST = fetch_strategy(inFolder)
+
+    scoreKeeper = {}
     for strategy in STRATEGY_LIST:
         scoreKeeper[strategy] = 0
         
     f = open(outFile,"w+")
     for pair in itertools.combinations(STRATEGY_LIST, r=2):
-        roundHistory = runRound(pair)
+        roundHistory = runRound(inFolder, pair)
         scoresA, scoresB = tallyRoundScores(roundHistory)
-        outputRoundResults(f, pair, roundHistory, scoresA, scoresB)
+        
         scoreKeeper[pair[0]] += scoresA
         scoreKeeper[pair[1]] += scoresB
+
+        outputRoundResults(f, pair, roundHistory, scoresA, scoresB)
         
     scoresNumpy = np.zeros(len(scoreKeeper))
     for i in range(len(STRATEGY_LIST)):
@@ -116,5 +123,34 @@ def runFullPairingTournament(inFolder, outFile):
     f.close()
     print("Done with everything! Results file written to "+RESULTS_FILE)
     
-    
-runFullPairingTournament(STRATEGY_FOLDER, RESULTS_FILE)
+def runSinglePairingTournament(inFolder, outFile, pair):
+    """
+    pair = list of strategy being evaluated
+    """
+
+    scoreKeeper = {}
+    for strategy in pair:
+        scoreKeeper[strategy] = 0
+
+    roundHistory = runRound(inFolder, pair)
+    scoresA, scoresB = tallyRoundScores(roundHistory)
+    scoreKeeper[pair[0]] += scoresA
+    scoreKeeper[pair[1]] += scoresB
+
+    f = open(outFile,"w+")
+    outputRoundResults(f, pair, roundHistory, scoresA, scoresB)
+
+    f.flush()
+    f.close()
+    print("Done with everything! Results file written to "+RESULTS_FILE)
+
+if __name__ == "__main__":
+    STRATEGY_FOLDER = "exampleStrats"
+    RESULTS_FILE = "results.txt"
+
+    ## FULL PAIRING TOURNAMENT:
+    # runFullPairingTournament(STRATEGY_FOLDER, RESULTS_FILE)
+
+    ## SINGLE PAIRING TOURNAMENT:
+    pair = ["detective", "ftft"]
+    runSinglePairingTournament(STRATEGY_FOLDER, RESULTS_FILE, pair)
