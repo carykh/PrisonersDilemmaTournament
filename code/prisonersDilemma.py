68704f0eb952f542c059f67588725786e96c208e
bs2kbs2k <10294621+bs2kbs2k@users.noreply.github.com>
2021-05-20 14:23:47 +0900

    nevermind average-of-N is already there I'm dumb

--- code/prisonersDilemma.py
+++ code/prisonersDilemma.py
@@ -172,7 +172,6 @@ def runFullPairingTournament(inFolders, outFile, summaryFile):
     rankings = np.argsort(scoresNumpy)
 
     mainFile.write("\n\nTOTAL SCORES\n")
-    averageScoreData = []  # for average-of-N calculations
     for rank in range(len(STRATEGY_LIST)):
         i = rankings[-1 - rank]
         score = scoresNumpy[i]
@@ -188,7 +187,6 @@ def runFullPairingTournament(inFolders, outFile, summaryFile):
         )
         mainFile.write(scoreLine)
         summaryFile.write(scoreLine)
-        averageScoreData.append((STRATEGY_LIST[i], scorePer))
 
     mainFile.flush()
     mainFile.close()
