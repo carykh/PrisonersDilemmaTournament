4bc8420e772a20a58ee230812de99ef4c173d0bf
Edward Haas <efhiii2000@gmail.com>
2021-05-19 23:00:42 -0700

    Remove old forgiving tit-fot-tat and stuff

--- code/prisonersDilemma.py
+++ code/prisonersDilemma.py
@@ -27,7 +27,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-STRATEGY_FOLDERS = ["exampleStrats", "valadaptive", "nekiwo", "etc"]
+STRATEGY_FOLDERS = ["exampleStrats", "valadaptive", "nekiwo", "edward", "etc"]
 if args.use_slow:
     STRATEGY_FOLDERS.append("slow")
 RESULTS_FILE = "results.txt"
