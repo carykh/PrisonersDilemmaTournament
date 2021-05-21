35d4b6d9360f8593d6e6f71a6279042af6d251a9
Ariam <44091391+Ariam27@users.noreply.github.com>
2021-05-21 15:29:37 +0530

    first choice changed to cooperate

--- code/misc/Ariam_trust.py
+++ code/misc/Ariam_trust.py
@@ -5,7 +5,7 @@ import random
 def strategy(history, memory):
     choice = None
     if history.shape[1] == 0:
-        choice = 0
+        choice = 1
     else:
         choice = random.choice(history[1])
     return choice, None
