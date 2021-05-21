348a538aaf8f93809312b04c6a17b35868f09b9d
Phoenix <PhoenixWT@protonmail.com>
2021-05-21 11:14:20 -0400

    Update my "agent" strat and add "periodicCopycat"

--- code/phoenix/agent.py
+++ code/phoenix/agent.py
@@ -1,18 +1,19 @@
 import random
 import numpy as np
 
-def strategy(history, _):
+def strategy(history, bad):
     choice = 1
 
-    if history.shape[1] != 0:
-        # Enemy just defected
-        if history[1, -1] == 0:
-            choice = 0
-            if history.shape[1] % 20 < 2:
-                choice = 1
-        else:
-            start = history.shape[1] - 4
-            end = history.shape[1]
-            random.choice(history[1, start:end])
+    if history.shape[1] == 0:
+        return 1, 0
 
-    return choice, _
+    # Enemy just defected
+    if history[1, -1] == 0:
+        bad += 1
+        choice = 0
+    elif bad > history[0].sum():
+        choice = 0
+    else:
+        choice = random.choice(history[1, -7:])
+
+    return choice, bad
