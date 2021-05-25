c24a53d35273cbe3689c848c1b4fc5e3f721a3e4
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-05-25 17:47:04 -0500

    improvements against random and alwaysdefect

--- code/nekiwo/PrisonerSherlock.py
+++ code/nekiwo/PrisonerSherlock.py
@@ -5,17 +5,20 @@ import numpy as np
 
 # First it starts detecting random and fixes itself when it realizes its joss
 # If the opponent isnt random, it just does AntiJoss
-# Right before the end it aggressively goes nuts
+
+# If opponent's first move is defect, sherlock goes full defect
+
 
 # Config
-window = 8
+window = 8 
+EndRound = 200
 
 # Detecting
 class detection:
 
     def random(history, IsRandom):
         ComplyHistory = np.count_nonzero(history[1, -window:] == 1)
-        if ComplyHistory > 3 and ComplyHistory < 5: # Super easy way of doing it, works good enough
+        if ComplyHistory > np.floor(window / 3) and ComplyHistory < window: # Super easy way of doing it, works good enough
             # It's random
             IsRandom = True
 
@@ -36,17 +39,16 @@ class detection:
 # Doing strats
 class strat:
 
+    # Regular nice Joss
     def AntiJoss(history):
         choice = 1
-        if history.shape[1] >= 1 and history[1, -1] == 0:
+
+        if history.shape[1] > 0 and history[1, -1] == 0:
             choice = 0
             if random.random() < 0.10:
                 choice = 1
-        return choice
 
-    def GameEndAbuse():
-        
-        return 1
+        return choice
 
 
 def strategy(history, memory):
@@ -54,6 +56,7 @@ def strategy(history, memory):
     # Some memory variables
     IsRandom = False
     IsTFT = False
+    IsntNice = False
 
     choice = strat.AntiJoss(history) # Default choice is AntiJoss
 
@@ -62,20 +65,28 @@ def strategy(history, memory):
         # Memory variables in array:
         memory = [
                 IsRandom, # 0
-                IsTFT     # 1
+                IsTFT,    # 1
+                IsntNice  # 2
             ]
     else:
         IsRandom = memory[0]
         IsTFT = memory[1]
+        IsntNice = memory[2]
             
 
     # Enemy analyzis:
 
+    
+    # Opponent's first move was defect, start defecting
+    ## NOTE: change to first 3
+    if history.shape[1] == 4 and np.count_nonzero(history[1, -3:] == 1) == 0 or IsntNice:
+        IsntNice = True
+        choice = 0
+
     # Detect random
-    if history.shape[1] == window + 1:
+    elif history.shape[1] == window + 1:
         RandomDetector = detection.random(history, IsRandom)
         IsRandom = RandomDetector
-
 
     # Check if the "random" is actually Joss and revert back if it is
     elif history.shape[1] == window * 2 + 1 and IsRandom:
@@ -95,10 +106,7 @@ def strategy(history, memory):
         choice = 0
 
 
-
-
-
     # Saving memory variables before the next move
-    memory = [IsRandom, IsTFT]
+    memory = [IsRandom, IsTFT, IsntNice]
 
     return choice, memory
